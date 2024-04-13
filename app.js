ab32df749d080c5354ffc8d9a5ba02b45f50707e
KiraAme <77437133+KiraAme@users.noreply.github.com>
2024-04-13 15:23:02 +0200

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-function signup(){
-    document.location.href="signup.html";
-}
-function login(){
-    document.location.href="Login.html";
-}
-function index(){
-    document.location.href="index.html"
-}
