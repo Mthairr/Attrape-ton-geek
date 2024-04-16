000836ccbe7827e535e08b6e7ce307081f135b0c
Mathéo <131164608+Mthairr@users.noreply.github.com>
2024-04-16 19:57:54 +0200

    localStorage js create an account

--- js/signup.js
+++ js/signup.js
@@ -7,3 +7,29 @@ for (var i=18; i<=120; i++){
     option.value = i;
     SelectAge.add(option);
 }
+document.getElementById('SignupForm').addEventListener('submit', function(event){
+    event.preventDefault();
+
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    var confirmPassword = document.getElementById('confirmPassword').value;
+    var gender = document.getElementById('gender').value;
+    var age = document.getElementById('age').value;
+    var remember = document.getElementById('remember').checked;
+
+    if(password !== confirmPassword){
+        alert("Les mots de passe ne correspondent pas !");
+        return;
+    }
+    var utilisateur = {
+        username: username,
+        password: password,
+        gender: gender,
+        age: age,
+        remember: remember,
+    };
+    var utilisateurJSON = JSON.stringify(utilisateur);
+
+    localStorage.setItem('utilisateur', utilisateurJSON);
+    window.location.href="Bienvenue.html";
+});
