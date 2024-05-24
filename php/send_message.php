2563804c3a5702f3c2fa729014467ce2ed387d5a
MatgamFR <80062640+MatgamFR@users.noreply.github.com>
2024-05-24 16:15:32 +0200

    Add files via upload

--- php/send_message.php
+++ php/send_message.php
@@ -13,12 +13,23 @@
         if(strpos($tab[1], '%69') !== false) {
             $tab[1] = str_replace('%69', ';', $tab[1]);
         }
-        if(($tab[2] == $_SESSION["username"] && $tab[3] == $_GET["username"])){
-            echo "<br><p id=" . $tab[0] . ">" . $_SESSION["username"] . " : " . $tab[1] . "</p>";
+        if($_SESSION["admin"] = 1){
+            if(($tab[2] == $_SESSION["username"] && $tab[3] == $_GET["username"])){
+                echo "<br><p id=" . $tab[0] . ">" . $_SESSION["username"] . " : " . $tab[1] . "<button class='report-btn' onclick='suppr(this);'>supprimer</button></p>";
+            }
+            else if(($tab[2]==$_GET["username"] && $tab[3] == $_SESSION["username"])){
+                echo "<br><p id=" . $tab[0] . " class=mouse style='color:red;'>" . $_GET["username"] . " : " . $tab[1] . "<button class='report-btn' onclick='suppr(this);'>supprimer</button></p>";
+            }
         }
-        else if(($tab[2]==$_GET["username"] && $tab[3] == $_SESSION["username"])){
-            echo "<br><p id=" . $tab[0] . " class=mouse style='color:red;' onmouseover='handleMouseOver(this)' onmouseleave='handleMouseLeave(this)' onclick='signal(this);'>" . $_GET["username"] . " : " . $tab[1] . "</p>";
+        else{
+            if(($tab[2] == $_SESSION["username"] && $tab[3] == $_GET["username"])){
+                echo "<br><p id=" . $tab[0] . ">" . $_SESSION["username"] . " : " . $tab[1] . "</p>";
+            }
+            else if(($tab[2]==$_GET["username"] && $tab[3] == $_SESSION["username"])){
+                echo "<br><p id=" . $tab[0] . " class=mouse style='color:red;' onmouseover='handleMouseOver(this)' onmouseleave='handleMouseLeave(this)' onclick='signal(this);'>" . $_GET["username"] . " : " . $tab[1] . "</p>";
+            }
         }
+        
     }
 ?>
 
