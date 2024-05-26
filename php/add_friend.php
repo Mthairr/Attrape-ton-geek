a9798c05b8c4aee1862763b2cf35a6afb7dedbee
KiraAme <77437133+KiraAme@users.noreply.github.com>
2024-05-26 16:08:49 +0200

    Add files via upload

--- php/add_friend.php
+++ php/add_friend.php
@@ -1,45 +1,29 @@
 <?php
 session_start();
-
-if(count($_COOKIE) > 0){
-    if(empty($_SESSION["name"])){
+if (count($_COOKIE) > 0) {
+    if (empty($_SESSION["name"])) {
         header('Location: page3.php');
     }
-}
-else{
+}else {
     header('Location: ../index.php');
 }
 
-if (!isset($_POST['ami_username'])) {
-    echo json_encode(["success" => false, "message" => "Aucun utilisateur spécifié."]);
-    exit;
-}
 
-$ami_username = $_POST['ami_username'];
-$utilisateur_connecte = $_SESSION['username'];
-$chemin_fichier_amis = "../donnee/friends.txt";
-
-$amis_utilisateur_connecte = [];
-if (file_exists($chemin_fichier_amis)) {
-    $amis_utilisateur_connecte = file($chemin_fichier_amis, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-} else {
-    file_put_contents($chemin_fichier_amis, '');
-}
-
-// Vérifie si l'ami est déjà ajouté
-foreach ($amis_utilisateur_connecte as $ligne) {
-    $champs = explode(';', $ligne);
-    if ($champs[0] === $utilisateur_connecte && $champs[1] === $ami_username) {
-        echo json_encode(["success" => false, "message" => "Ami déjà ajouté."]);
-        header("Location: ../profil.php?username=$ami_username");
-        exit;
+$fileLines = file('../donnee/friends.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+for($i=0; $i<count($fileLines); $i++){
+    $message = explode(";", $fileLines[$i]);
+    if($message[1] == $_GET['username'] && $message[0] == $_SESSION['username']){
+        unset($fileLines[$i]);
+        file_put_contents("../donnee/friends.txt", implode("", $fileLines));
+        header('Location: ../profil.php?username=' . $_GET['username']);
+        exit();
     }
 }
 
-// Ajoute l'ami
-$amis_utilisateur_connecte[] = $utilisateur_connecte . ';' .  $ami_username;
-file_put_contents($chemin_fichier_amis, implode("\n", $amis_utilisateur_connecte));
 
+$amis_utilisateur_connecte[] = $_SESSION['username'] . ';' .  $_GET['username'];
+file_put_contents('../donnee/friends.txt', implode("\n", $amis_utilisateur_connecte));
+$ami_username=$_GET['username'];
 $response = array(
     "success" => true,
     "ami_username" => $ami_username
@@ -49,4 +33,3 @@ echo json_encode($response);
 
 header("Location: ../profil.php?username=$ami_username");
 exit();
-?>
