b194f4cf728ddf231d180e4cad015ea5a8acb3ce
MatgamFR <80062640+MatgamFR@users.noreply.github.com>
2024-05-26 22:21:42 +0200

    Add files via upload

--- php/verification_abonnement.php
+++ php/verification_abonnement.php
@@ -5,9 +5,8 @@
         exit();
     }
     $now = new DateTime();
-    $then = $_SESSION["abonnement"];
+    $then = new DateTime($_SESSION["abonnement"]);
     if($then < $now){
-        $_SESSION["abonnement"] = 0;
         $username = $_SESSION['username'];
         $password = $_SESSION["password"];
         $age = $_SESSION["age"];
@@ -18,9 +17,13 @@
         $ville = $_SESSION['town'];
         $pays = $_SESSION['country'];
         $adresse = $_SESSION['adress'];
+        $character = $_SESSION['character'];
+        $game = $_SESSION['game'];
+        $type_game = $_SESSION['type_game'];
         $target_gender = $_SESSION['target_gender'];
         $height = $_SESSION['height'];
         $eyes = $_SESSION['eyes'];
+        $description = $_SESSION['description'];
         $lignes = file('../donnee/log.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
         $nouvelle_lignes = [];
 
@@ -28,27 +31,31 @@
             $champs = explode(';', $ligne);
             if ($champs[0] === $_SESSION["username"]) {
                 $nouvelle_ligne = implode(';', [
-                    $username,
-                    $password,
-                    $age,
-                    $sexualindentity,
-                    $email,
-                    $prenom,
-                    $nom,
-                    $adresse,
-                    $ville,
-                    $pays,
-                    $height,
-                    $eyes,
-                    $target_gender,
-                    "0"
+                $username,
+                $password,
+                $age,
+                $sexualindentity,
+                $email,
+                $prenom,
+                $nom,
+                $adresse,
+                $ville,
+                $pays,
+                $character,
+                $game,
+                $type_game,
+                $height,
+                $eyes,
+                $target_gender,
+                $description,
+                "0"
                 ]);
                 $nouvelle_lignes[] = $nouvelle_ligne;
             } else {
                 $nouvelle_lignes[] = $ligne;
             }
         }
-        file_put_contents('../donnee/log.txt', implode("\n", $nouvelle_lignes) . ";");
+        file_put_contents('../donnee/log.txt', implode("\n", $nouvelle_lignes));
     }
     else{
         http_response_code(400);
