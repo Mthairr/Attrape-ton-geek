b1474472de62e05326502eddb550ad9802a8aa5d
MatgamFR <80062640+MatgamFR@users.noreply.github.com>
2024-05-26 18:00:16 +0200

    Add files via upload

--- view_ban.php
+++ view_ban.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="css/profil.css">
 </head>
 <body>
-<h1>View_reports</h1>
+<h1>View Ban</h1>
 <?php
     session_start();
 
@@ -21,13 +21,59 @@
         header('Location: index.php');
     }
 
+    $file = fopen("donnee/ban.txt", 'r');
+    $reports = [];
 
-    $ban = file_get_contents('donnee/ban.txt');
+    // Lire le fichier ligne par ligne
+    while (($line = fgets($file)) !== false) {
+        // Supprimer les espaces blancs en début et fin de ligne
+        $line = trim($line);
 
-    $ban = nl2br($ban);
+        // Ignorer les lignes vides
+        if (empty($line)) {
+            continue;
+        }
 
-    echo "<h2>Bannissements</h2>";
-    echo "<pre>$ban</pre>";
+        // Séparer les données en utilisant le séparateur ";"
+        $data = explode(';', $line);
+
+        // Vérifier que chaque ligne contient le bon nombre de données
+        if (count($data) === 3) {
+            // Ajouter les données au tableau des bans
+            $reports[] = [
+                'email' => $data[0],
+                'raison' => $data[1]
+            ];
+        } else {
+            echo "La ligne suivante ne contient pas le bon nombre de données : $line\n";
+        }
+    }
+
+    // Fermer le fichier
+    fclose($file);
+
+    $html = '<table border="1">';
+    $html .= '<tr>';
+    $html .= '<th>Adresse email</th>';
+    $html .= '<th>Raison du ban</th>';
+    $html .= '</tr>';
+
+    // Ajouter les lignes du tableau avec les données des signalements
+    foreach ($reports as $report) {
+        $html .= '<tr>';
+        $html .= '<td>' . htmlspecialchars($report['email']) . '</td>';
+        $html .= '<td>' . htmlspecialchars($report['raison']) . '</td>';
+        $html .= '</tr>';
+    }
+
+    // Fermer le tableau HTML
+    $html .= '</table>';
+
+
+
+    echo "<h2>Ban</h2>";
+    echo $html;
+
 
 ?>
 
